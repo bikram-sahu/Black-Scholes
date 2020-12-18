7191c51d2a132950f70aa0a08d6824b70808d545
Bikram Keshari Sahu <bikramsahu1997@gmail.com>
2020-12-18 22:10:25 +0530

    Added delta-hedging, Volatility Smile, VaR

--- BSM_app.py
+++ BSM_app.py
@@ -1,10 +1,13 @@
+from numpy.lib.shape_base import column_stack
 import streamlit as st
 import numpy as np
 from scipy.stats import norm
 import pandas as pd
 import os, urllib
 import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
 import seaborn as sns
+import yfinance as yf
 sns.set()
 
 
@@ -27,14 +30,21 @@ def main():
     # Once we have the dependencies, add a selector for the app mode on the sidebar.
     st.sidebar.title("Black-Scholes")
     app_mode = st.sidebar.selectbox("Choose the app mode",
-                                    ["Select", "Option Price", "Monte Carlo", "Option Greeks", "Show the source code"])
+                                    ["Select", "Derivation of Black-Scholes", "Option Pricing", "Option Greeks", "Monte Carlo",
+                                     "IV vs Strike Price", "Delta Hedging", "VaR", "Show the source code"])
     
     if app_mode == "Select":
         st.sidebar.success('To continue select from the dropdown.')
+    elif app_mode == "Derivation of Black-Scholes":
+        readme_text.empty()
+        st.markdown(get_file_content_as_string("BSM_equation_derivation.md"))
+        with st.beta_expander('Assumptions of Black-Scholes'):
+            st.markdown(get_file_content_as_string("BSM_assupmtions.md"))
+
     elif app_mode == "Show the source code":
         readme_text.empty()
         st.code(display_source_code("BSM_app.py"))
-    elif app_mode == "Option Price":
+    elif app_mode == "Option Pricing":
         readme_text.empty()
         run_option_price()
     elif app_mode == "Option Greeks":
@@ -43,6 +53,16 @@ def main():
     elif app_mode == "Monte Carlo":
         readme_text.empty()
         run_monte_carlo()
+    elif app_mode == "VaR":
+        readme_text.empty()
+        #value_at_risk()
+        st.markdown('Under Development')
+    elif app_mode == "IV vs Strike Price":
+        readme_text.empty()
+        iv_vs_strike()
+    elif app_mode == "Delta Hedging":
+        readme_text.empty()
+        delta_hedging()
 
     return None
 
@@ -552,18 +572,18 @@ def run_monte_carlo():
 
     if var == "Algorithm":
        st.markdown(get_file_content_as_string("monte_carlo_algo.md"))
-       st.markdown("[Click here to access the Jupyter notebook of the code implemented.](https://github.com/bikram-sahu/Black-Scholes/blob/main/Monte-Carlo-BSM.ipynb)")
+       st.markdown("[Click here to access the Jupyter notebook of the code implemented.](https://github.com/bikram-sahu/Black-Scholes/blob/main/jupyter_notebooks/Monte-Carlo-BSM.ipynb)")
        st.markdown(
            "Select **Implementation** from dropdown box to see the results of Monte Carlo Simulation.")
     elif var == "Implementation":
         st.write('**Enter the parameter values below!**')
         col1, col2, col3 = st.beta_columns(3)
         #opt_type = col1.selectbox("Option type", ['call', 'put'])
-        S0 = col1.number_input('Spot')
-        K = col2.number_input('Strike')
-        T = col3.number_input('Time-to-Maturity')
-        sigma = col1.number_input('Volatility')
-        r = col2.number_input('Risk-free rate')
+        S0 = col1.number_input('Spot', 110)
+        K = col2.number_input('Strike', 100)
+        T = col3.number_input('Time-to-Maturity', 0.5)
+        sigma = col1.number_input('Volatility', 0.25)
+        r = col2.number_input('Risk-free rate', 0.05)
         
         def simulate_stock_price(S0, r, sigma, dt, M, I):
             # Simulating I paths with M time step
@@ -576,19 +596,199 @@ def run_monte_carlo():
         I = st.sidebar.slider('Number of Simulations', 100000, 250000)
         S = simulate_stock_price(S0, r, sigma, dt, M, I)
         S[0] = S0
-        if col2.button('Calculate'):
+        #if col2.button('Calculate'):
         
-            # Calculating the Monte Carlo estimator
-            C0 = np.exp(-r * T) * sum(np.maximum(S[-1] - K, 0)) / I
-            st.write('Option Price Calculated by Monte Carlo Value is:', C0)
-            exact_C0 = black_scholes(S0, K, T, r, sigma, "call")
-            st.write('Option Price Calculated from Analytical formula is:', exact_C0)
+        # Calculating the Monte Carlo estimator
+        C0 = np.exp(-r * T) * sum(np.maximum(S[-1] - K, 0)) / I
+        exact_C0 = black_scholes(S0, K, T, r, sigma, "call")
+        ind = ["Analytical", "Monte Carlo", "Difference"]
+        df = pd.DataFrame(data = [exact_C0, C0, np.abs(exact_C0 - C0)], index = ind, columns = ["Option Price"] )
+        st.table(df)
 
-            fig, ax1 = plt.subplots()
-            plt.plot(S[:, :10])
-            ax1.set(xlabel='Steps', ylabel='Stock Price',
-                    title='Simulated Stock Price Using Euler Method.')
-            st.pyplot(fig)
+        fig, ax1 = plt.subplots()
+        plt.plot(S[:, :10])
+        ax1.set(xlabel='Steps', ylabel='Stock Price',
+                title='Simulated Stock Price Using Euler Method.')
+        st.pyplot(fig)
+
+
+def value_at_risk():
+
+    st.markdown(get_file_content_as_string("var.md"))
+    col1, col2, col3 = st.beta_columns(3)
+    ticker_sym = col1.text_input('Enter a ticker symbol')
+    start_date = col2.date_input('Start date')
+    end_date = col3.date_input('End date')
+    if col2.button('Calculate'):
+        df = yf.download(ticker_sym, start_date, end_date)
+        df = df[['Close']]
+        df['returns'] = df.Close.pct_change()
+
+        mean = np.mean(df['returns'])
+        std_dev = np.std(df['returns'])
+
+        df['returns'].hist(bins=40, normed=True, histtype='stepfilled', alpha=0.5)
+        x = np.linspace(mean - 3*std_dev, mean + 3*std_dev, 100)
+        fig, ax1 = plt.subplots()
+        plt.plot(x, mlab.normpdf(x, mean, std_dev), "r")
+        ax1.set()
+        st.pyplot(fig)
+
+
+def iv_vs_strike():
+    st.markdown(
+        r'''
+        ## Volatility Surface
+        The incorrectness of Black-Scholes is most obviously manifested through the volatility surface
+        - a concept that is found throughout derivatives markets.
+        The volatility surface is constructed using market prices of European call and put options
+        - can also use American prices but it's a little trickier.
+
+        ### Definition. 
+        The volatility surface, $\sigma(K, T),$ is a function of the strike, $K,$ and expiration, $T$. 
+        It is defined implicitly by
+        $$
+        \large
+        C_{\mathrm{mkt}}(S, K, T)=C_{\mathrm{BS}}(S, T, r, c, K, \sigma(K, T))
+        $$
+
+        where $C_{\mathrm{mkt}}(S, K, T)$ denotes the market price of the call option with expiration, $T,$ 
+        and strike, $K,$ and $C_{\mathrm{BS}}(\cdot)$ is the corresponding Black-Scholes formula for pricing 
+        this call option.
+
+        ## Volatility Smile
+
+        A volatility smile is a common graph shape that results from plotting the strike price and implied 
+        volatility of a group of options with the same underlying asset and expiration date. The volatility 
+        smile is so named because it looks like a smiling mouth. Implied volatility rises when the underlying 
+        asset of an option is further out of the money (OTM) or in the money (ITM), compared to at the money (ATM).
+
+        ''')
+    #data = pd.read_excel('strike-vs-iv.xlsx')
+    file = st.file_uploader('Upload a file containg STRIKE PRICE & IV')
+    if file:
+        data = pd.read_excel(file)
+        fig, ax = plt.subplots()
+        data.plot('STRIKE PRICE', 'IV', kind='line',
+                    label='Implied Volatility', ax=ax)
+        ax.set(xlabel='Strike Price', ylabel='Implied Volatility',
+                title='IV vs Strike Price for Nifty')
+        st.pyplot(fig)
+
+def delta_hedging():
+    #st.markdown(get_file_content_as_string("delta-hedging.md"))
+    st.markdown(
+    r'''
+    Let us now view the option price as a function of $S$ and $\sigma$ only.
+    A simple application of Taylor's Theorem says
+    $$
+    \large
+    \begin{aligned} 
+    C(S+\Delta S, \sigma+\Delta \sigma) & \approx C(S, \sigma)+\Delta S \frac{\partial C}{\partial S}+\frac{1}{2}(\Delta S)^{2} \frac{\partial^{2} C}{\partial S^{2}}+\Delta \sigma \frac{\partial C}{\partial \sigma} \\
+    &=C(S, \sigma)+\Delta S \delta+ \frac{1}{2}(\Delta S)^{2} \Gamma+\Delta \sigma \text { vega. }
+    \end{aligned}
+    $$
+    We therefore obtain
+    $$
+    \large
+    \begin{aligned}
+    \mathrm{P} \& \mathrm{~L} & \approx \delta \Delta S+\frac{\Gamma}{2}(\Delta S)^{2}+\text { vega } \Delta \sigma \\
+    &=\text { delta } \mathrm{P} \& \mathrm{~L}+\text { gamma } \mathrm{P} \& \mathrm{~L}+\text { vega } \mathrm{P} \& \mathrm{~L}
+    \end{aligned}
+    $$
+    Can also write
+    $$
+    \large
+    \begin{aligned}
+    \mathrm{P} \& \mathrm{~L} & \approx \delta S\left(\frac{\Delta S}{S}\right)+\frac{\Gamma S^{2}}{2}\left(\frac{\Delta S}{S}\right)^{2}+\text { vega } \Delta \sigma \\
+    &=\mathrm{ESP} \times \text { Return }+\$ \text { Gamma } \times \text { Return }^{2}+\text { vega } \Delta \sigma
+    \end{aligned}
+    $$
+    where ESP denotes the equivalent stock position or "dollar" delta.
+    ''')
+    with st.beta_expander('Example'):
+        notional = 100
+        Delta = [0.5]
+        Gamma = [0.13]
+        Vega = [1.2]
+        Spot = [100]
+        Strike = 100
+        Sigma = [16]
+        col1, col2, col3, col4 = st.beta_columns(4)
+        with col1:
+            st.write('Notional =', notional)
+            st.write('Delta =', Delta[0])
+            st.write('Gamma = ', Gamma[0])
+            st.write('Vega = ', Vega[0])
+        with col2:
+            st.write('Spot = ', Spot[0])
+            st.write('Strike =', Strike)
+            st.write('Sigma =', Sigma[0])
+        with col3:
+            st.write('Futures sold', -1*notional*Delta[0])
+        
+        st.markdown('## Case 1')
+        Delta.append(0.6)
+        Gamma.append(0.09)
+        Vega.append(0.8)
+        Spot.append(105)
+        Sigma.append(17)
+        deltaS = (Spot[1] - Spot[0])
+        col1, col2, col3, col4 = st.beta_columns(4)
+        with col1:
+            st.write('Delta =', Delta[1])
+            st.write('Gamma = ', Gamma[1])
+            st.write('Vega = ', Vega[1])
+        with col2:
+            st.write('Spot = ', Spot[1])
+            st.write('Strike =', Strike)
+            st.write('Sigma = ', Sigma[1])
+        with col3:
+            st.write('Futures sold', -1*notional*Delta[1])
+            st.write('Future PnL', -1*notional*Delta[0]*deltaS)
+        with col4:
+            #st.info('Option PnL')
+            
+            st.write('Delta PnL', Delta[0]*notional* deltaS)
+            st.write('Gamma PnL', 0.5* Gamma[0]* notional * deltaS**2)
+            st.write('Vega PnL', Vega[0]*notional* (Sigma[1] - Sigma[0]))
+
+        st.markdown('## Case 2')
+        Delta.append(0.5)
+        Gamma.append(0.13)
+        Vega.append(1.2)
+        Spot.append(100)
+        Sigma.append(16)
+        deltaS = (Spot[2] - Spot[1])
+        col1, col2, col3, col4 = st.beta_columns(4)
+        with col1:
+            st.write('Delta =', Delta[2])
+            st.write('Gamma = ', Gamma[2])
+            st.write('Vega = ', Vega[2])
+        with col2:
+            st.write('Spot = ', Spot[2])
+            st.write('Strike =', Strike)
+            st.write('Sigma = ', Sigma[2])
+        with col3:
+            st.write('Futures sold', -1*notional*Delta[2])
+            st.write('Future PnL', -1*notional*Delta[1]*deltaS)
+        with col4:
+            #st.info('Option PnL')
+
+            st.write('Delta PnL', Delta[1]*notional * deltaS)
+            st.write('Gamma PnL', 0.5 * Gamma[1] * notional * deltaS**2)
+            st.write('Vega PnL', Vega[1]*notional * (Sigma[2] - Sigma[1]))
+
+
+
+        
+
+        
+
+
+
+
+
 
 
 if __name__ == "__main__":
